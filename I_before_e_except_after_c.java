b14e6613890303259cee2c85ff25fc5c46651d0c
nkonjeti <nkonjeti@gmail.com>
2019-11-14 19:18:13 -0600

    Update I_before_e_except_after_c.java

--- I_before_e_except_after_c.java
+++ I_before_e_except_after_c.java
@@ -1,10 +1,5 @@
 
-/**
- * Write a description of class I_before_e_except_after_c here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
+
 import java.util.Scanner;
 public class I_before_e_except_after_c
 {
