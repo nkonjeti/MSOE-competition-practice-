fadbae2caeaaa3fc903deebdc714aeaf41a1a128
nkonjeti <nkonjeti@gmail.com>
2019-11-14 19:16:02 -0600

    Add files via upload

---
+++ I_before_e_except_after_c.java
@@ -0,0 +1,31 @@
+
+/**
+ * Write a description of class I_before_e_except_after_c here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+public class I_before_e_except_after_c
+{
+   public static void main (String args[]) {
+       
+       Scanner s = new Scanner(System.in);
+       System.out.println("Enter sentence");
+       String sentence = s.nextLine();
+       String[] words = sentence.split(" ");
+       int count = 1;
+       for (String x: words) {
+           if (x.contains("ei")) {
+              if (!x.contains("cei")) {
+                  System.out.println(count + " : " + x);
+                }
+            }
+            count++;
+        
+           
+        }
+    }   
+    
+}
+
